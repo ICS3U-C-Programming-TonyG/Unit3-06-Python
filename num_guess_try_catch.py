b897a93db231cc89c2bc0e32d6dad5275bb226ce
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-03-28 00:40:11 +0000

    fixed issues

--- num_guess_try_catch.py
+++ num_guess_try_catch.py
@@ -4,9 +4,11 @@
 # Date: 2025-03-27
 # Asks for user input and tells user if number is correct or not, loops until correct
 
+import random
 def main():
     # Greeting message
     print("Greetings! Try to guess the correct number!")
+    Random_number = random.randint(0, 9)
 
     # Infinitely loops program until user guesses correctly
     while True:
@@ -15,15 +17,15 @@ def main():
             user_input = input("Please enter a number: ")
             # Convert input to an integer
             number = int(user_input)
-            if number == 5:
+            if number == Random_number:
                 print("Congratulations! Correct!")
                 # "breaks" loop if correct
-                break  
+                break
             else:
-                print("Incorrect, try again :(")
+                print("Incorrect, try again :( Integer was " + str(Random_number))
         # Exception
         except ValueError:
-            print("Invalid input! Please enter an integer.")
+            print(user_input + " is not a integer. Enter an integer.")
 
 if __name__ == "__main__":
     main()
